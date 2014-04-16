f339b45c950ce603f13bd70d51d35f706b101310
HMHamster88 <hmhamster88@gmail.com>
2014-04-16 10:01:23 +0400

    Update

--- GCodeGen.py
+++ GCodeGen.py
@@ -22,14 +22,28 @@ def  v2_from_angle(angle):
 class GCodeGenerator:
 	horizontal_feed = 100
 	vertial_feed = 100
+	safety_height = 15
 	def __init__(self, file):
 		self.file = file
 
+	def  abs_coords(self):
+		self.file.write("G90")
+
+	def go_to_safety_height(self):
+		self.file.write("G00 Z{0:0.4f}\n".format(self.safety_height))
+	
+	def go_to(self, v):
+		self.file.write("G00 X{0:0.4f} Y{1:0.4f}\n".format(v.x, v.y))
+
 	def horizontal_feed_to(self, v):
-		self.file.write("G01 X{0} Y{1} F{2}\n".format(v.x, v.y, self.horizontal_feed))
+		self.file.write("G01 X{0:0.4f} Y{1:0.4f} F{2:0.4f}\n".format(v.x, v.y, self.horizontal_feed))
 
 	def vertical_feed_to(self, z):
-		self.file.write("G01 Z{0} F{1}\n".format(z, self.vertial_feed))
+		self.file.write("G01 Z{0:0.4f} F{1:0.4f}\n".format(z, self.vertial_feed))
+
+	def go_to_arc_start(self, center, radius, start_angle, stop_angle, cw):
+		start = center + v2_from_angle(start_angle) * radius;
+		self.go_to(start)
 
 	def horizontal_arc(self, center, radius, start_angle, stop_angle, cw):
 		start = center + v2_from_angle(start_angle) * radius;
@@ -40,4 +54,4 @@ class GCodeGenerator:
 			self.file.write("G02")
 		else:
 			self.file.write("G03")
-		self.file.write(" X{0} Y{1} I{2} J{3} F{4}\n".format(end.x, end.y, start_offset.x, start_offset.y, self.horizontal_feed))
+		self.file.write(" X{0:0.4f} Y{1:0.4f} I{2:0.4f} J{3:0.4f} F{4:0.4f}\n".format(end.x, end.y, start_offset.x, start_offset.y, self.horizontal_feed))
--- pulley.py
+++ pulley.py
@@ -1,20 +1,28 @@
 import GCodeGen
 import math
 
-file = open('test.nc', 'w')
+file = open('pulley.nc', 'w')
 codeGen = GCodeGen.GCodeGenerator(file)
-#codeGen.horizontal_arc(GCodeGen.v2(0, 0), 10, 0, 3.14*1.5, 1)
 
-tool_radius = 1
+def drange(start, end, step):
+    while (step > 0 and start <= end) or (step < 0 and start >= end):
+        yield start
+        start += step
+    if (step > 0 and start - step < end) or (step < 0 and start + step > end):
+    	yield end;
+
+tool_radius = (25.4 * 0.0591) / 2
 
 socket_radius = 2
 socket_deep = 1.6
-tooth_count = 60
+tooth_count = 66
 tooth_angle = math.pi * 2 / tooth_count
 socket_angle = tooth_angle * (25.6211 / 36)
-pulley_radius = tooth_count * 1.45
+pulley_radius = tooth_count * 1.5 #1.45 - 10 tooth
 fillet_r = 0.5
 edges = []
+
+print(pulley_radius)
 
 a = pulley_radius - socket_deep
 b = pulley_radius - fillet_r
@@ -24,9 +32,12 @@ u = math.acos((b*b + k*k - a*a)/(2*b*k))
 fillet_angle = math.pi - (u + z);
 socket_ang = math.pi - (fillet_angle - socket_angle / 2)
 
-for z in range(2, 10):
-	codeGen.vertical_feed_to(z)
-	for tooth in range(0, tooth_count):
+codeGen.abs_coords()
+codeGen.go_to_safety_height()
+
+for z in drange(11, 11-5, -1):
+	codeGen.go_to_safety_height()
+	for tooth in range(0, 5):
 		tooth_a_begin = tooth * tooth_angle;
 		tooth_a_end = (tooth + 1) * tooth_angle - socket_angle;
 		tooth_a_next = (tooth + 1) * tooth_angle;
@@ -43,9 +54,12 @@ for z in range(2, 10):
 		tooth_pos1 = GCodeGen.v2_from_angle(tooth_a_next) * pulley_radius
 		tooth_pos2 = GCodeGen.v2_from_angle(tooth_a_end) * pulley_radius
 
+		codeGen.go_to_arc_start(GCodeGen.v2(0, 0), pulley_radius + tool_radius, tooth_a_begin, tooth_a_end, 0)
+		codeGen.vertical_feed_to(z)
 		codeGen.horizontal_arc(GCodeGen.v2(0, 0), pulley_radius + tool_radius, tooth_a_begin, tooth_a_end, 0)
 		codeGen.horizontal_arc(fillet_pos1, fillet_r + tool_radius, tooth_a_end, tooth_a_end + fillet_angle, 0)
 
 		codeGen.horizontal_arc(socket_pos, socket_radius - tool_radius, socket_a_center - socket_ang, socket_a_center + socket_ang, 1)
 
 		codeGen.horizontal_arc(fillet_pos2, fillet_r + tool_radius, tooth_a_next - fillet_angle, tooth_a_next, 0)
+codeGen.go_to_safety_height()
